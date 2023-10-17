98373a3ed95715f4077e744d0c884de33059bcb9
LastRoman10 <89813782+LastRoman10@users.noreply.github.com>
2023-10-17 07:17:47 +0100

    Initialize 37-terratowns-provider

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+# output "bucket_name" {
+#     value = random_string.bucket_name.result
+# }
+
+# output "bucket_name" {
+#     value = aws_s3_bucket.example
+# }
+
+### This outputs the full details of the bucket
+# output "bucket_name" {
+#     value = aws_s3_bucket.website_bucket
+# }
+
+
+
+
+### This outputs the bucket name only, it's an attribute os the s3 resource
+# output "bucket_name" {
+#     value = module.terrahouse_aws.bucket_name
+# }
+
+# output "website_endpoint" {
+#     value = module.terrahouse_aws.s3_website_endpoint
+# }
+
+# output "cloudfront_url" {
+#     description = "Cloudfront Distribution Domain Name"
+#     value = module.terrahouse_aws.cloudfront_url
+# } 
---
+++ providers.tf
@@ -0,0 +1,30 @@
+## In the terraform block we specify the provider we require
+
+
+# terraform {
+#   cloud {
+#     organization = "Pauly_DevOps"
+
+#     workspaces {
+#       name = "TerraHouse-Red"
+#     }
+#   }
+# }
+
+## In the provider block we specify the configurations for the provider we chose. It's we optional
+
+terraform {
+  required_providers {
+    terratowns = {
+        source = "local.providers/local/terratowns"
+        version = "1.0.0"
+    }
+  }
+}
+
+provider "terratowns" {
+    endpoint = "http://localhost:4567/api"  
+    user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+    token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  
+}
